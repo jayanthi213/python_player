1f15f4182c01069855422ced49dfbd04bc9bb839
jayanthi213 <32953938+jayanthi213@users.noreply.github.com>
2017-10-21 12:06:59 +0530

    Add files via upload

---
+++ Odd or Even.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece013
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece013 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+   a=5
+   if(a%2==0):
+        print("number is even")
+   else:
+        print("nuber is odd")
+
+if __name__ == '__main__':
+    main()
