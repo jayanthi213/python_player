2a448c051f4c5ebb6f3461020423df0c0f3ed3ec
jayanthi213 <32953938+jayanthi213@users.noreply.github.com>
2017-10-21 11:56:57 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece013
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece013 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def my_function():
+    print "Hello From My Function!"
+
+def my_function_with_args(first,sec):
+    print first + sec
+
+
+my_function()
+my_function_with_args("Your name ", "have a great year!")
+
