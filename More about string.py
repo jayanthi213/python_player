ad7dc85e593fffa9030d3e7e4b6b1d84ddbaa0e7
jayanthi213 <32953938+jayanthi213@users.noreply.github.com>
2017-10-21 11:01:24 +0530

    Add files via upload

---
+++ More about string.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece013
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece013 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    mystring='programmer guvi'
+    print(mystring.find('g'))
+    print(mystring.replace('m','y'))
+    print(mystring[1:4])
+    print(mystring.split(" "))
+if __name__ == '__main__':
+    main()
