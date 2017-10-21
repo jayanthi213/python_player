2b256719c6efb440acd0f1579768ea1a4ed4bcba
jayanthi213 <32953938+jayanthi213@users.noreply.github.com>
2017-10-21 10:10:33 +0530

    Add files via upload

---
+++ Basic string.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece013
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece013 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    mystring='programmer'
+    print len(mystring)
+if __name__ == '__main__':
+    main()
